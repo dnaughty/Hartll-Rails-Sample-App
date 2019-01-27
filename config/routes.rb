5f60a9943e50afa31571e7ceb13ba13b7fddb6bf
DENIS NAUGHTON <mjolinor75@hotmail.comssh-keygen -t rsa -b 4096 -C mjolinor75@hotmail.com>
2019-01-27 16:41:08 -0500

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,9 @@ Rails.application.routes.draw do
 
   get '/signup',  to: 'users#new'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/signup', to: 'users#create'
 
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
   
 end
